17715c33777f47a7fba87a4c4287715732a62c2a
Nikola Slavnic <nslavnic@theblockchainxdev.com>
2022-05-31 22:43:07 +0200

    first commit

---
+++ autostart.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+#lxsession &
+#picom &
+#dwmblocks &
+#feh --bg-fill '/home/nslavnic/Pictures/ZE9ZvL4.png'
+#/usr/bin/emacs --daemon &
+### Uncomment only ONE of the following ###
+# uncomment this line to restore last saved wallpaper...
+#xargs xwallpaper --stretch < ~/.xwallpaper &
+# ...or uncomment this line to set a random wallpaper on login
+#find /usr/share/backgrounds/dtos-backgrounds/ -type f | shuf -n 1 | xargs xwallpaper --stretch &
